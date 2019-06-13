d050d39e0c61ab7991fae51ae0d12d1fa4c0d96f
Yang <pourplusquoi@icloud.com>
2019-06-12 23:44:59 -0700

    饶了我吧编译器

---
+++ trie.rs
@@ -0,0 +1,96 @@
+use std::borrow::Borrow;
+use std::borrow::BorrowMut;
+use std::boxed::Box;
+use std::option::Option;
+
+struct Node {
+    in_dict: bool,
+    children: Vec<Option<Box<Node>>>,
+}
+
+impl Node {
+    fn new() -> Self {
+        let mut node = Node {
+            in_dict: false,
+            children: Vec::new(),
+        };
+
+        for _ in 0..256 {
+            node.children.push(None);
+        }
+
+        node
+    }
+}
+
+struct Trie {
+    root: Option<Box<Node>>,
+}
+
+impl Trie {
+    fn new() -> Self {
+        Trie {
+            root: Some(Box::new(Node::new())),
+        }
+    }
+
+    fn insert(&mut self, word: &str) -> () {
+        Trie::insert_inl(&mut self.root, word);
+    }
+
+    fn find(&self, word: &str) -> bool {
+        Trie::find_inl(&self.root, word)
+    }
+
+    fn insert_inl(node: &mut Option<Box<Node>>, remain: &str) -> () {
+        let node_mut: &mut Node = node.as_mut().unwrap().borrow_mut();
+        if remain.is_empty() {
+            node_mut.in_dict = true;
+        } else {
+            let index = remain.chars().next().unwrap() as usize;
+            let child = &mut node_mut.children[index];
+            if child.is_none() {
+                *child = Some(Box::new(Node::new()));
+            }
+            Trie::insert_inl(child, &remain[1..]);
+        }
+    }
+
+    fn find_inl(node: &Option<Box<Node>>, remain: &str) -> bool {
+        match node {
+            &Some(ref node_) => {
+                let inner: &Node = node_.borrow();
+                if remain.is_empty() {
+                    inner.in_dict
+                } else {
+                    let index = remain.chars().next().unwrap() as usize;
+                    Trie::find_inl(&inner.children[index], &remain[1..])
+                }
+            }
+            &None => false
+        }
+    }
+}
+
+fn main() {
+    let mut trie = Trie::new();
+
+    let dictionary = vec![String::from("apple"), String::from("banana")];
+    for word in &dictionary {
+        trie.insert(word);
+    }
+
+    let query = String::from("apple");
+    if trie.find(&query) {
+        println!("{} is found in dictionary", &query);
+    } else {
+        println!("{} not found", &query);
+    }
+
+    let query = String::from("pineapple");
+    if trie.find(&query) {
+        println!("{} exists in dictionary", &query);
+    } else {
+        println!("{} is not found", &query);
+    }
+}
