f4ccfe89bd164777e7c7229e5434a232b684d522
Yang Pan <pourplusquoi@icloud.com>
2019-11-07 00:20:09 -0800

    Update 1192.rs

--- leetcode/1192.rs
+++ leetcode/1192.rs
@@ -8,7 +8,7 @@
 //
 // Return all critical connections in the network in any order.
 //
-// Ex.
+// Example:
 // Input: n = 4, connections = [[0,1],[1,2],[2,0],[1,3]]
 // Output: [[1,3]]
 // Explanation: [[3,1]] is also accepted.
