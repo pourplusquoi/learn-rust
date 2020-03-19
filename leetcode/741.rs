3f4a72bb35ed91b442cabb04dac26fb422cfd221
Yang Pan <pourplusquoi@icloud.com>
2020-03-19 00:49:30 -0700

    Update 741.rs

--- leetcode/741.rs
+++ leetcode/741.rs
@@ -1,4 +1,5 @@
 use std::cmp::max;
+use std::cmp::min;
 use std::collections::HashSet;
 
 impl SolutionTLE {
@@ -47,7 +48,7 @@ impl SolutionTLE {
   }
 }
 
-impl SolutionUglyDP {
+impl SolutionTopDownUglyDP {
   pub fn cherry_pickup(grid: Vec<Vec<i32>>) -> i32 {
     let n = grid.len();
     let mut dp = vec![vec![vec![-2; n]; n]; n];
@@ -85,7 +86,7 @@ impl SolutionUglyDP {
     
     if entry >= 0 {
       entry += grid[r1][c1];
-      if r1 != r2 || c1 != c2 {
+      if r1 != r2 {
         entry += grid[r2][c2];
       }
     } else {
@@ -94,5 +95,48 @@ impl SolutionUglyDP {
     
     dp[r1][c1][r2] = entry;
     entry
+  }
+}
+
+impl SolutionBottomUpDP {
+  pub fn cherry_pickup(grid: Vec<Vec<i32>>) -> i32 {
+    let n = grid.len();
+    let mut dp = vec![vec![vec![-2; n]; n]; n];
+    for i in 0..n {
+      for j in 0..n {
+        let lo = max(0, j as i32 - i as i32) as usize;
+        let hi = min(n + j - i, n);
+        for k in lo..hi {
+          if i == 0 && j == 0 && k == 0 {
+            dp[0][0][0] = grid[0][0];
+            continue;
+          }
+          let l = i - j + k;
+          let mut entry = -2;
+          if i > 0 && j > 0 && grid[i - 1][k] > -1 && grid[j - 1][l] > -1 {
+            entry = max(entry, dp[i - 1][j - 1][k]);
+          }
+          if i > 0 && l > 0 && grid[i - 1][k] > -1 && grid[j][l - 1] > -1 {
+            entry = max(entry, dp[i - 1][j][k]);
+          }
+          if k > 0 && j > 0 && grid[i][k - 1] > -1 && grid[j - 1][l] > -1 {
+            entry = max(entry, dp[i][j - 1][k - 1]);
+          }
+          if k > 0 && l > 0 && grid[i][k - 1] > -1 && grid[j][l - 1] > -1 {
+            entry = max(entry, dp[i][j][k - 1]);
+          }
+          if entry >= 0 {
+            entry += grid[i][k];
+            if i != j {
+              entry += grid[j][l];
+            }
+          } else {
+            entry = -1;
+          }
+          dp[i][j][k] = entry;
+        }
+      }
+    }
+    max(0, dp[n - 1][n - 1][n - 1])
   }
 }
