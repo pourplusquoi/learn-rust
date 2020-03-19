cb379b58f7f8ce93b5bdd41219cf2577c729e6d1
Yang Pan <pourplusquoi@icloud.com>
2020-03-19 13:04:34 -0700

    Update 956.rs

--- leetcode/956.rs
+++ leetcode/956.rs
@@ -1,5 +1,26 @@
 use std::cmp::max;
 
+impl SolutionDP1D {
+  pub fn tallest_billboard(rods: Vec<i32>) -> i32 {
+    let sum = rods.iter().sum::<i32>() as usize;
+    let len = sum * 2 + 1;
+    let mut dp = vec![-1; len];
+    dp[sum] = 0;
+    for r in rods.iter() {
+      let mut cp = dp.to_vec();
+      for s in 0..len {
+        if (dp[s] < 0) { continue; }
+        let lo = s - *r as usize;
+        let hi = s + *r as usize;
+        cp[lo] = max(cp[lo], *r + dp[s]);
+        cp[hi] = max(cp[hi], dp[s]);
+      }
+      dp = cp;
+    }
+    dp[sum]
+  }
+}
+
 impl SolutionDP2D {
   pub fn tallest_billboard(rods: Vec<i32>) -> i32 {
     let sum = rods.iter().sum::<i32>() as usize;
@@ -20,26 +41,5 @@ impl SolutionDP2D {
       }
     }
     dp[n][sum]
-  }
-}
-
-impl SolutionDP1D {
-  pub fn tallest_billboard(rods: Vec<i32>) -> i32 {
-    let sum = rods.iter().sum::<i32>() as usize;
-    let len = sum * 2 + 1;
-    let mut dp = vec![-1; len];
-    dp[sum] = 0;
-    for r in rods.iter() {
-      let mut cp = dp.to_vec();
-      for s in 0..len {
-        if (dp[s] < 0) { continue; }
-        let lo = s - *r as usize;
-        let hi = s + *r as usize;
-        cp[lo] = max(cp[lo], *r + dp[s]);
-        cp[hi] = max(cp[hi], dp[s]);
-      }
-      dp = cp;
-    }
-    dp[sum]
   }
 }
