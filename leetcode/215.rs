40eedc58aea7713e5c9a02cc28c7d593e4812ecf
Yang Pan <pourplusquoi@icloud.com>
2020-03-22 01:30:43 -0700

    Update 215.rs

--- leetcode/215.rs
+++ leetcode/215.rs
@@ -40,3 +40,42 @@ impl Solution_QuickSelect_TwoWay {
     lo
   }
 }
+
+impl Solution_QuickSelect_ThreeWay {
+  pub fn find_kth_largest(mut nums: Vec<i32>, k: i32) -> i32 {
+    let mut lo = 0;
+    let mut hi = nums.len();
+    let mut idx = (k - 1) as usize;
+    while (lo < hi) {
+      let (begin, end) = Self::partition(&mut nums[lo..hi], lo);
+      if begin <= idx && idx < end {
+        return nums[begin];
+      }
+      if idx < begin {
+        hi = begin;
+      } else {
+        lo = end;
+      }
+    }
+    // Never reach here.
+    std::i32::MIN
+  }
+  
+  fn partition(nums: &mut [i32], offset: usize) -> (usize, usize) {
+    let target = nums[0];
+    let mut begin = 0;  // The begin of eq.
+    let mut end = 1;    // The end of eq.
+    for idx in 1..nums.len() {
+      if nums[idx] == target {
+        nums.swap(idx, end);
+        end += 1;
+      } else if nums[idx] > target {
+        nums.swap(idx, begin);
+        nums.swap(idx, end);
+        begin += 1;
+        end += 1;
+      }
+    }
+    (begin + offset, end + offset)
+  }
+}
