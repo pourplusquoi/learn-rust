bdc25fcc227bb555aa6ccaeeecb911a4538cef78
Yang Pan <pourplusquoi@icloud.com>
2020-03-22 21:56:43 -0700

    Update 1386.rs

--- leetcode/1386.rs
+++ leetcode/1386.rs
@@ -5,8 +5,8 @@ impl Solution {
     let mut j = 0;
     for i in 0..n {
       let mut mask = 0;
-      while reserved[j][0] == i {
-        mask += 1 << (reserved[j][1] as usize - 1);
+      while j < reserved.len() && reserved[j][0] - 1 == i {
+        mask += 1 << (reserved[j][1] - 1);
         j += 1;
       }
       if mask & 30 == 0 && mask & 480 == 0 {
